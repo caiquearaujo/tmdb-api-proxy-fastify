42847498ed550e5df3bb79c7fe68b870cef1be56
Caique Araujo <caique@studiopiggly.com.br>
2023-02-01 00:02:53 -0300

    :sparkles: feat(proxy): Include session_id on request params

--- src/www/plugins/proxy.plugin.ts
+++ src/www/plugins/proxy.plugin.ts
@@ -5,6 +5,18 @@ const callable: TFnApplyToFastify = async (app, env) => {
 	await app.register(fastifyProxy, {
 		upstream: 'https://api.themoviedb.org/3',
 		prefix: '/api',
+		preHandler(request, reply, next) {
+			if (request.headers.cookie) {
+				const { session_id } = app.parseCookie(request.headers.cookie);
+				const unsignedSessionId = app.unsignCookie(session_id);
+
+				if (unsignedSessionId.value) {
+					request.params = { session_id: unsignedSessionId.value };
+				}
+			}
+
+			next();
+		},
 		replyOptions: {
 			rewriteRequestHeaders: (req, headers) => ({
 				...headers,
